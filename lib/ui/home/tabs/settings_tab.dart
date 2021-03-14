d1863506e14f534b50c65639050474115c53efe6
zuludin <zmauludin04@gmail.com>
2021-03-14 21:00:13 +0700

    use SharedPreferences to save notification status

--- lib/ui/home/tabs/settings_tab.dart
+++ lib/ui/home/tabs/settings_tab.dart
@@ -1,5 +1,6 @@
 import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:warung_makan/core/utils/background_service.dart';
 import 'package:warung_makan/core/utils/date_time_helper.dart';
 
@@ -9,7 +10,15 @@ class SettingsTab extends StatefulWidget {
 }
 
 class _SettingsTabState extends State<SettingsTab> {
-  bool _isActive = false;
+  Future<SharedPreferences> _preferences = SharedPreferences.getInstance();
+  Future<bool> _isActive;
+
+  @override
+  void initState() {
+    _isActive = _preferences
+        .then((value) => value.getBool('NOTIFICATION_STATUS') ?? false);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +27,24 @@ class _SettingsTabState extends State<SettingsTab> {
         Material(
           child: ListTile(
             title: Text('Recommend Restaurant'),
-            trailing: Switch.adaptive(value: _isActive, onChanged: _changeNotification),
+            trailing: FutureBuilder<bool>(
+              future: _isActive,
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return const CircularProgressIndicator();
+                  default:
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      return Switch.adaptive(
+                        value: snapshot.data,
+                        onChanged: _changeNotification,
+                      );
+                    }
+                }
+              },
+            ),
           ),
         )
       ],
@@ -38,6 +64,11 @@ class _SettingsTabState extends State<SettingsTab> {
     } else {
       await AndroidAlarmManager.cancel(1);
     }
-    setState(() => _isActive = active);
+
+    var prefs = await _preferences;
+    setState(() {
+      _isActive =
+          prefs.setBool('NOTIFICATION_STATUS', active).then((value) => active);
+    });
   }
 }
